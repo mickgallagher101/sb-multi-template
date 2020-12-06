73ffd7f039f72deb265c2d7c2cf83ca8a01d7cea
Mick <mickster.gallagher@gmail.com>
2020-12-06 13:32:52 -0800

    Fixing the library unit test with injection and context missing

--- sb-library-one/src/test/java/com/micks/pmlib/LibraryTest.java
+++ sb-library-one/src/test/java/com/micks/pmlib/LibraryTest.java
@@ -5,11 +5,24 @@ package com.micks.pmlib;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
+@AutoConfigureWebTestClient(timeout = "36000")
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {Library.class})
+@SpringBootTest
 public class LibraryTest {
+
+    @Autowired
+    private ApplicationContext context;
 
     @Autowired
     Library library;

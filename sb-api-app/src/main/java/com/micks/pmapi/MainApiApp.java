2683c62e88aec7d8804b8cbfdab5bc3c8cc48a36
Mick <mickster.gallagher@gmail.com>
2020-12-06 16:33:00 -0800

    UPdating readme

--- sb-api-app/src/main/java/com/micks/pmapi/MainApiApp.java
+++ sb-api-app/src/main/java/com/micks/pmapi/MainApiApp.java
@@ -1,13 +1,13 @@
 package com.micks.pmapi;
 
-import com.micks.pmlib1.LibraryOne;
-import com.micks.pmlib2.LibraryTwo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.micks.pmlib1.LibraryOne;
+import com.micks.pmlib2.LibraryTwo;
 
 /**
  * sb-multi-template - Class description
